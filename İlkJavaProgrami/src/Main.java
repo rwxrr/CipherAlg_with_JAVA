ed3a432716bcd268e7c2f6729c6d86c4973616cd
rwxrr <zeynep.klc19o3@gmail.com>
2016-06-28 19:18:09 -0400

    İnitial commit

---
+++ İlkJavaProgrami/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+	
+	Scanner in = new Scanner(System.in);
+	args=new String[2];
+	args[0]=in.next();
+	System.out.println("Adı :" + args[0]);
+	
+	}
+}
